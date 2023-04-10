3af02f13f23c80da541567746b6e5a8e47d24188
Laurel McLean <laurel.mclean@live.ca>
2023-04-10 17:35:26 -0600

    Users can submit and save posts

---
+++ controllers/posts.js
@@ -0,0 +1,22 @@
+const Post = require('../models/post');
+
+module.exports = (app) => {
+
+    // NEW
+    app.get('/posts/new', (req, res) => {
+        res.render('posts-new')
+    });
+
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+
+        // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
+        post.save()
+            .then(() => {
+                res.redirect('/')
+            })
+            .catch(err => console.log(err))
+    });
+};
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
+// Require Libraries
+const express = require('express');
+const handlebars = require('express-handlebars');
 
+// App Setup
 const app = express();
+app.use(express.static('public'));
 
-app.engine('handlebars', engine());
+// db setup
+require('./data/reddit-db');
+
+// Middleware
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Require controllers
+require('./controllers/posts')(app)
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
 });
 
 app.listen(3000);
