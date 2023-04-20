f7c7f31af4444684ec02e1457c2fa52dfd9827ab
Laurel McLean <laurel.mclean@live.ca>
2023-04-20 12:23:37 -0600

    users can login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-
 module.exports = (app) => {
 
     // SIGN UP FORM
@@ -15,11 +14,9 @@ module.exports = (app) => {
 
             // Create JWT token
             const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
-
             // Set cookie
             res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
             res.redirect('/');
-
         } catch (err) {
             if (err.name === 'MongoError' && err.code === 11000) {
                 // Duplicate key error - username already taken
@@ -33,7 +30,39 @@ module.exports = (app) => {
     // LOGOUT
     app.get('/logout', (req, res) => {
         res.clearCookie('nToken');
-        return res.redirect('/');
+        res.redirect('/');
+    });
+
+    // LOGIN FORM
+    app.get('/login', (req, res) => res.render('login'));
+
+    //LOGIN
+    app.post('/login', async (req, res) => {
+        const { username, password } = req.body;
+        try {
+            // Find this user name
+            const user = await User.findOne({ username }, 'username password');
+            if (!user) {
+                // User not found
+                return res.status(401).send({ message: 'Wrong Username or Password' });
+            }
+            // Check the password
+            user.comparePassword(password, (err, isMatch) => {
+                if (!isMatch) {
+                    // Password does not match
+                    return res.status(401).send({ message: 'Wrong Username or password' });
+                }
+                // Create a token
+                const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                    expiresIn: '60 days',
+                });
+                // Set a cookie and redirect to root
+                res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+                return res.redirect('/');
+            });
+        } catch (err) {
+            console.log(err);
+        }
     });
 
 };
