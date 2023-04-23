1c43df87a9af8597fa8f97ebb732b3423948dd7a
Laurel McLean <laurel.mclean@live.ca>
2023-04-22 20:54:40 -0600

    fixed replies yay

--- controllers/replies.js
+++ controllers/replies.js
@@ -1,3 +1,4 @@
+
 const Post = require('../models/post');
 const User = require('../models/user');
 const Comment = require('../models/comment');
@@ -7,7 +8,7 @@ module.exports = (app) => {
     app.get('/posts/:postId/comments/:commentId/replies/new', async (req, res) => {
         try {
             const currentUser = req.user;
-            const post = await Post.findById(req.params.postId).lean()
+            const post = await Post.findById(req.params.postId).lean();
             const comment = await Comment.findById(req.params.commentId).lean();
             res.render('replies-new', { post, comment, currentUser });
         } catch (err) {
@@ -22,15 +23,17 @@ module.exports = (app) => {
             const reply = new Comment(req.body);
             reply.author = req.user._id;
             // LOOKUP THE PARENT POST
-            const post = await Post.findById(req.params.postId)
+            const post = await Post.findById(req.params.postId);
             // FIND THE CHILD COMMENT
-            const comment = await Comment.findById(req.params.commentId);
-            comment.comments.unshift(reply._id);
+            const [savedReply, comment] = await Promise.all([
+                reply.save(),
+                Comment.findById(req.params.commentId),
+            ]);
+            // ADD THE REPLY
+            comment.comments.unshift(savedReply._id);
             await comment.save();
-
             // SAVE THE CHANGE TO THE PARENT DOCUMENT
             await post.save();
-
             res.redirect(`/posts/${req.params.postId}`);
         } catch (err) {
             console.error(err);
